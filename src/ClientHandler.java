aeb931c99f1d062a353594491203cd4916e7611c
Grzegorz Listwan <listwan94@gmail.com>
2023-12-11 11:09:28 +0100

    add ClientHandler class

---
+++ src/ClientHandler.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+private Socket clientSocket;
+private  int clientNumber;
+
+public ClientHandler(Socket clientSocket, int clientNumber){
+    this.clientSocket = clientSocket;
+    this.clientNumber = clientNumber;
+}
+    @Override
+    public void run() {
+        try{
+            BufferedReader fileReader = new BufferedReader(new FileReader("s.txt"));
+            PrintWriter outToClient = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader inFromCLient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String line;
+            while ((line = fileReader.readLine())!=null){
+                outToClient.println(line);
+                String response = inFromCLient.readLine();
+                System.out.println("Klient "+clientNumber+" odpowiedzial: "+response);
+
+                try(PrintWriter writer = new PrintWriter(new FileWriter("c.txt",true))){
+                    writer.println(response);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            fileReader.close();
+            inFromCLient.close();
+            outToClient.close();
+            clientSocket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/Serwer.java
+++ src/Serwer.java
@@ -1,2 +1,23 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Serwer {
+    public static void main(String[] args) {
+        try{
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Serwer nasłuchuje ....");
+            int clientCount = 0;
+            while (clientCount <250){
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Połączono z klientem "+ (clientCount+1));
+                Thread clientThread = new Thread(new ClientHandler(clientSocket, clientCount));
+                clientThread.start();
+                clientCount++;
+            }
+            serverSocket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
